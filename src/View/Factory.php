7e3e8892ed9860e62a1f3a40e49e1abf5ac77b93
Asif Iqbal <muhammad.asif@abtach.org>
2016-05-24 10:20:36 +0000

    Added custom view

---
+++ src/View/Factory.php
@@ -0,0 +1,53 @@
+<?php namespace Webwizo\Shortcodes\View;
+
+use Closure;
+use Illuminate\Events\Dispatcher;
+use Illuminate\View\ViewFinderInterface;
+use Illuminate\View\Engines\EngineResolver;
+use Illuminate\View\Factory as IlluminateViewFactory;
+use Webwizo\Shortcodes\Compilers\ShortcodeCompiler;
+
+class Factory extends IlluminateViewFactory {
+
+    /**
+     * Short code engine resolver
+     *  @var ShortcodeCompiler
+     */
+    public $shortcode;
+
+    /**
+     * Create a new view factory instance.
+     *
+     * @param \Illuminate\View\Compilers\EngineResolver|EngineResolver $engines
+     * @param  \Illuminate\View\ViewFinderInterface                    $finder
+     * @param  \Illuminate\Events\Dispatcher                           $events
+     * @param ShortcodeCompiler                                        $shortcode
+     */
+    public function __construct(EngineResolver $engines, ViewFinderInterface $finder, Dispatcher $events, ShortcodeCompiler $shortcode)
+    {
+        parent::__construct($engines, $finder, $events);
+        $this->shortcode = $shortcode;
+    }
+
+    /**
+     * Get the evaluated view contents for the given view.
+     *
+     * @param  string $view
+     * @param  array  $data
+     * @param  array  $mergeData
+     *
+     * @return \Illuminate\Contracts\View\View|string|View
+     */
+    public function make($view, $data = array(), $mergeData = array())
+    {
+        if (isset($this->aliases[$view])) $view = $this->aliases[$view];
+
+        $path = $this->finder->find($view);
+
+        $data = array_merge($mergeData, $this->parseData($data));
+
+        $this->callCreator($view = new View($this, $this->getEngineFromPath($path), $view, $path, $data, $this->shortcode));
+
+        return $view;
+    }
+}
---
+++ src/View/View.php
@@ -0,0 +1,80 @@
+<?php namespace Webwizo\ShortCodes\View;
+
+use ArrayAccess;
+use Closure;
+use Illuminate\Support\MessageBag;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\View\View as IlluminateView;
+use Illuminate\View\Engines\EngineInterface;
+use Webwizo\Shortcodes\Compilers\ShortcodeCompiler;
+
+class View extends IlluminateView implements ArrayAccess, Renderable {
+
+    /**
+     * Short code engine resolver
+     *  @var \Webwizo\Shortcodes\View\ShortcodeCompiler
+     */
+    public $shortcode;
+
+    /**
+     * Create a new view instance.
+     *
+     * @param \Illuminate\View\Factory|Factory                           $factory
+     * @param \Illuminate\View\Compilers\EngineInterface|EngineInterface $engine
+     * @param  string                                                    $view
+     * @param  string                                                    $path
+     * @param  array                                                     $data
+     * @param ShortcodeCompiler                                          $shortcode
+     */
+    public function __construct(Factory $factory, EngineInterface $engine, $view, $path, $data = array(), ShortcodeCompiler $shortcode)
+    {
+        parent::__construct($factory, $engine, $view, $path, $data);
+        $this->shortcode = $shortcode;
+    }
+
+    /**
+     * Enable the shortcodes
+     */
+    public function withShortcodes()
+    {
+        $this->shortcode->enable();
+        return $this;
+    }
+
+    /**
+     * Disable the shortcodes
+     */
+    public function withoutShortcodes()
+    {
+        $this->shortcode->disable();
+        return $this;
+    }
+
+    /**
+     * Get the contents of the view instance.
+     *
+     * @return string
+     */
+    protected function renderContents()
+    {
+        // We will keep track of the amount of views being rendered so we can flush
+        // the section after the complete rendering operation is done. This will
+        // clear out the sections for any separate views that may be rendered.
+        $this->factory->incrementRender();
+
+        $this->factory->callComposer($this);
+
+        $contents = $this->getContents();
+
+        // compile the shortcodes
+        $contents = $this->shortcode->compile($contents);
+
+        // Once we've finished rendering the view, we'll decrement the render count
+        // so that each sections get flushed out next time a view is created and
+        // no old sections are staying around in the memory of an environment.
+        $this->factory->decrementRender();
+
+        return $contents;
+    }
+
+}
